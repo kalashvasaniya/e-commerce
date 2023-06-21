57c2e262aaa10c641641792143c89311ed35a159
kalash <kalashvasaniya@gmail.com>
2023-06-22 04:20:37 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'HomeoStore',
+  description: 'E-Commerce Website for users!',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+      </head>
+      <body className={inter.className}>
+        {children}
+        <div className="bg-black">
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+      </body>
+    </html>
+  )
+}
