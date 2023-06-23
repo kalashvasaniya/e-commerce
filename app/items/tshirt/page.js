716ff0f7d05e781a1b11bb2b52fb0b330ab45374
kalash <kalashvasaniya@gmail.com>
2023-06-24 03:08:47 +0530

    23 -Finally side navbar

--- app/items/tshirt/page.js
+++ app/items/tshirt/page.js
@@ -11,8 +11,8 @@ const Tshirt = () => {
             <div class="grid text sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 -m-4 gap-12">
 
               <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <Link href={'/product/kalash'} class="block relative h-80 rounded overflow-hidden">
-                  <img alt="ecommerce" class="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
+                <Link href={'/product/wear-the-tshirt'} class="block relative h-80 rounded overflow-hidden">
+                  <img alt="ecommerce" class=" object-cover object-top w-full h-full block hover:scale-110 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
                 </Link>
                 <div class="mt-4">
                   <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -22,75 +22,82 @@ const Tshirt = () => {
               </div>
 
               <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-80 rounded overflow-hidden">
-                  <img alt="ecommerce" class="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
-                </a>
+                <Link href={'/product/wear-the-tshirt'} class="block relative h-80 rounded overflow-hidden">
+                  <img alt="ecommerce" class=" object-cover object-top w-full h-full block hover:scale-110 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
+                </Link>
                 <div class="mt-4">
                   <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
                   <h2 class="text-gray-200 title-font text-lg font-medium">Shooting Stars</h2>
                   <p class="mt-1 text-blue-500">$21.15</p>
                 </div>
               </div>
+
               <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-80 rounded overflow-hidden">
-                  <img alt="ecommerce" class="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
-                </a>
+                <Link href={'/product/wear-the-tshirt'} class="block relative h-80 rounded overflow-hidden">
+                  <img alt="ecommerce" class=" object-cover object-top w-full h-full block hover:scale-110 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
+                </Link>
                 <div class="mt-4">
                   <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
                   <h2 class="text-gray-200 title-font text-lg font-medium">Neptune</h2>
                   <p class="mt-1 text-blue-500">$12.00</p>
                 </div>
               </div>
+
               <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-80 rounded overflow-hidden">
-                  <img alt="ecommerce" class="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
-                </a>
+                <Link href={'/product/wear-the-tshirt'} class="block relative h-80 rounded overflow-hidden">
+                  <img alt="ecommerce" class=" object-cover object-top w-full h-full block hover:scale-110 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
+                </Link>
                 <div class="mt-4">
                   <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
                   <h2 class="text-gray-200 title-font text-lg font-medium">The 400 Blows</h2>
                   <p class="mt-1 text-blue-500">$18.40</p>
                 </div>
               </div>
+
               <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-80 rounded overflow-hidden">
-                  <img alt="ecommerce" class="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
-                </a>
+                <Link href={'/product/wear-the-tshirt'} class="block relative h-80 rounded overflow-hidden">
+                  <img alt="ecommerce" class=" object-cover object-top w-full h-full block hover:scale-110 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
+                </Link>
                 <div class="mt-4">
                   <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
                   <h2 class="text-gray-200 title-font text-lg font-medium">The Catalyzer</h2>
                   <p class="mt-1 text-blue-500">$16.00</p>
                 </div>
               </div>
+
               <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-80 rounded overflow-hidden">
-                  <img alt="ecommerce" class="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
-                </a>
+                <Link href={'/product/wear-the-tshirt'} class="block relative h-80 rounded overflow-hidden">
+                  <img alt="ecommerce" class=" object-cover object-top w-full h-full block hover:scale-110 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
+                </Link>
                 <div class="mt-4">
                   <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
                   <h2 class="text-gray-200 title-font text-lg font-medium">Shooting Stars</h2>
                   <p class="mt-1 text-blue-500">$21.15</p>
                 </div>
               </div>
+
               <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-80 rounded overflow-hidden">
-                  <img alt="ecommerce" class="object-cover object-centtoper w-full h-full block" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
-                </a>
+                <Link href={'/product/wear-the-tshirt'} class="block relative h-80 rounded overflow-hidden">
+                  <img alt="ecommerce" class=" object-cover object-top w-full h-full block hover:scale-110 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
+                </Link>
                 <div class="mt-4">
                   <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
                   <h2 class="text-gray-200 title-font text-lg font-medium">Neptune</h2>
                   <p class="mt-1 text-blue-500">$12.00</p>
                 </div>
               </div>
+
               <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-80 rounded overflow-hidden">
-                  <img alt="ecommerce" class="object-cover object-top w-full h-full block" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
-                </a>
+                <Link href={'/product/wear-the-tshirt'} class="block relative h-80 rounded overflow-hidden">
+                  <img alt="ecommerce" class=" object-cover object-top w-full h-full block hover:scale-110 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" />
+                </Link>
                 <div class="mt-4">
                   <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
                   <h2 class="text-gray-200 title-font text-lg font-medium">The 400 Blows</h2>
                   <p class="mt-1 text-blue-500">$18.40</p>
                 </div>
               </div>
+
             </div>
           </div>
         </section>
--- components/navbar/page.js
+++ components/navbar/page.js
@@ -1,8 +1,21 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useRef } from 'react'
 
 const Navbar = () => {
+    const toggleCart = () => {
+        if (ref.current.classList.contains('-translate-x-full')) {
+            ref.current.classList.remove('-translate-x-full')
+            ref.current.classList.add('translate-x-0')
+        }
+        else {
+            ref.current.classList.remove('translate-x-0')
+            ref.current.classList.add('-translate-x-full')
+        }
+    }
+    const ref = useRef();
     return (
         <>
             <nav className="bg-black fixed w-full z-20 top-0 left-0 border-b">
@@ -16,20 +29,38 @@ const Navbar = () => {
 
                     <div className="md:order-4 inline-flex">
 
-                        {/* Cart section  */}
-                        <Link href={'/checkout'} type="button" className="hidden md:block relative items-center text-sm font-medium text-center text-white hover:scale-110">
+                        {/* side navbar  */}
+                        <div onClick={toggleCart} className="md:block hidden">
                             <button type="button" className="relative items-center p-2 text-sm font-medium text-center group bg-white rounded-full">
                                 <svg className='w-6 h-6 text-gray-400 group-hover:text-pink-500' viewBox="0 0 1024 1024">
                                     <path d="M922.9 701.9H327.4l29.9-60.9 496.8-.9c16.8 0 31.2-12 34.2-28.6l68.8-385.1c1.8-10.1-.9-20.5-7.5-28.4a34.99 34.99 0 00-26.6-12.5l-632-2.1-5.4-25.4c-3.4-16.2-18-28-34.6-28H96.5a35.3 35.3 0 100 70.6h125.9L246 312.8l58.1 281.3-74.8 122.1a34.96 34.96 0 00-3 36.8c6 11.9 18.1 19.4 31.5 19.4h62.8a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7h161.1a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7H923c19.4 0 35.3-15.8 35.3-35.3a35.42 35.42 0 00-35.4-35.2zM305.7 253l575.8 1.9-56.4 315.8-452.3.8L305.7 253zm96.9 612.7c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6zm325.1 0c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6z" /></svg>
                                 <span className="sr-only">Notifications</span>
                                 <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2">69</div>
                             </button>
-                        </Link>
+
+                            <div ref={ref} onClick={toggleCart} class="absolute top-0 left-0 z-60 w-64 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-gray-700" tabindex="-1">
+                                <h5 class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
+                                <button onClick={toggleCart} type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
+                                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    <span class="sr-only">Close menu</span>
+                                </button>
+                                <div class="py-4 overflow-y-auto">
+                                    <ul class="space-y-2 font-medium">
+                                        <li>
+                                            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                                <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+                                                <span class="ml-3">Dashboard</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
 
                         {/* Profile icon  */}
-                        <div className=" md:block hidden pl-6">
+                        <div className="md:block hidden pl-6">
 
-                            <Image src={'/logo.jpeg'} width={100} height={100} id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className="w-10 h-10 rounded-full cursor-pointer" alt="User dropdown" />
+                            <Image src={'/logo.jpeg'} width={100} height={100} id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className="w-10 h-10 rounded-full cursor-pointer hover:scale-110" alt="User dropdown" />
 
                             <div id="userDropdown" className="z-10 hidden divide-y rounded-lg shadow w-44 bg-gray-700 divide-gray-600">
                                 <div className="px-4 py-3 text-sm text-white">
@@ -41,13 +72,6 @@ const Navbar = () => {
                                 </div>
                             </div>
 
-                        </div>
-
-                        {/* side navbar  */}
-                        <div className="md:block hidden">
-                            <button className="text-gray-100 hover:ring-2 hover:ring-gray-200 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-2.5 ml-6" type="button" data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar">
-                                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                            </button>
                         </div>
 
                     </div>
@@ -98,8 +122,6 @@ const Navbar = () => {
                     </div>
                 </div>
 
-
-
                 <hr className='text-gray-100' />
                 {/* second top navbar */}
                 <div className="max-w-lg px-4 py-3 mx-auto align-middle">
@@ -128,10 +150,8 @@ const Navbar = () => {
                         <div className="md:hidden">&gt;</div>
                     </div>
                 </div>
+
             </nav>
-
-
-
 
             {/* bottom navbar */}
             <nav className='md:hidden'>
@@ -209,3 +229,14 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+{/* Cart section  */ }
+{/* <Link href={'/checkout'} type="button" className="hidden md:block relative items-center text-sm font-medium text-center text-white hover:scale-110">
+                            <button type="button" className="relative items-center p-2 text-sm font-medium text-center group bg-white rounded-full" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
+                                <svg className='w-6 h-6 text-gray-400 group-hover:text-pink-500' viewBox="0 0 1024 1024">
+                                    <path d="M922.9 701.9H327.4l29.9-60.9 496.8-.9c16.8 0 31.2-12 34.2-28.6l68.8-385.1c1.8-10.1-.9-20.5-7.5-28.4a34.99 34.99 0 00-26.6-12.5l-632-2.1-5.4-25.4c-3.4-16.2-18-28-34.6-28H96.5a35.3 35.3 0 100 70.6h125.9L246 312.8l58.1 281.3-74.8 122.1a34.96 34.96 0 00-3 36.8c6 11.9 18.1 19.4 31.5 19.4h62.8a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7h161.1a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7H923c19.4 0 35.3-15.8 35.3-35.3a35.42 35.42 0 00-35.4-35.2zM305.7 253l575.8 1.9-56.4 315.8-452.3.8L305.7 253zm96.9 612.7c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6zm325.1 0c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6z" /></svg>
+                                <span className="sr-only">Notifications</span>
+                                <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2">69</div>
+                            </button>
+</Link> */}
