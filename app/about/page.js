9a67809cdd8a6e37625a4170badeccf6328b29bf
kalash <kalashvasaniya@gmail.com>
2023-06-24 18:47:43 +0530

    About Page

--- app/about/page.js
+++ app/about/page.js
@@ -3,10 +3,19 @@ import React from 'react'
 const About = () => {
     return (
         <>
-            <div className="bg-black text-white h-screen pt-52">
-                <h1 className="text-4xl">About</h1>
-                <p className="text-2xl">This is the about page</p>
-            </div>
+            <section class="bg-gray-900 mt-52 rounded-2xl mx-8">
+                <div class="gap-16 items-center py-4 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+                    <div class="font-light text-gray-400 sm:text-lg ">
+                        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-white">We didn't reinvent the wheel</h2>
+                        <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+                        <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+                    </div>
+                    <div class="grid grid-cols-2 gap-4 mt-8">
+                        <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+                        <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
