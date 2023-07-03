12ff468f7c48250f623b5df890b6e57b7c4c6bc9
kalash <kalashvasaniya@gmail.com>
2023-07-03 10:31:21 +0530

    Checkout page - 3

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,12 +1,86 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const countries = ["China", "Russia", "UK"];
+  const [menu, setMenu] = useState(false);
+  const [country, setCountry] = useState("United States");
+
+  const changeText = (e) => {
+    setMenu(false);
+    setCountry(e.target.textContent);
+  };
+
   return (
     <>
-        <h1 className='pt-52 h-screen'>Checkout</h1>
+      <div className="pt-52 pb-8">
+        <h1 className="text-4xl font-bold text-white text-center">Check Out</h1>
+      </div>
 
+      <div className="md:grid md:grid-cols-2 px-6">
+
+        <div className="">
+          <div className="grid grid-cols-2 bg-gray-500 rounded-3xl p-4 max-w-xl m-auto mb-6">
+            <div className="flex flex-col justify-start space-y-4">
+              <p className="text-xl md:text-2xl leading-normal font-mono font-bold text-gray-100">Logitech K251</p>
+              <p className="text-base font-semibold leading-none text-gray-300">$520.00</p>
+            </div>
+            <div className="">
+              <img className="rounded-xl w-full h-56 object-cover object-top hover:scale-105 transition ease-in delay-300" src="https://m.media-amazon.com/images/I/61xFiBw2fNL._UY879_.jpg" alt="headphones" />
+            </div>
+          </div>
+        </div>
+
+
+        <div className="max-w-xl min-w-max m-auto">
+          {/* payment area  */}
+          <div className="p-8 bg-gray-100 flex flex-col rounded-3xl">
+
+            <div className="mt-8">
+              <input className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="Email" required/>
+            </div>
+
+            <label className="mt-8 text-base leading-4 text-gray-800">Card details</label>
+            <div className="mt-2 flex-col">
+              <div>
+                <input className="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="0000 1234 6549 15151" required/>
+              </div>
+              <div className="flex-row flex">
+                <input className="border rounded-bl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="MM/YY" required/>
+                <input className="border rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="CVC" required/>
+              </div>
+            </div>
+
+            <label className="mt-8 text-base leading-4 text-gray-800">Country or region</label>
+            <div className="mt-2 flex-col">
+              <div className="relative">
+                <button className="text-left border rounded-tr rounded-tl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600 bg-white" type="email">
+                  {country}
+                </button>
+                <svg onClick={() => setMenu(!menu)} className={"transform  cursor-pointer absolute top-4 right-4 " + (menu ? "rotate-180" : "")} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M3.5 5.75L8 10.25L12.5 5.75" stroke="#27272A" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+                <div className={"mt-1 absolute z-10 w-full flex bg-gray-50 justify-start flex-col text-gray-600 " + (menu ? "block" : "hidden")}>
+                  {countries.map((country) => (
+                    <div key={country} className="cursor-pointer hover:bg-gray-800 hover:text-white px-4 py-2" onClick={changeText}>
+                      {country}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <input className="border rounded-bl rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="text" placeholder="ZIP" />
+            </div>
+
+            <button className="mt-8 border border-transparent hover:border-gray-300 bg-gray-900 hover:bg-white text-white hover:text-gray-900 flex justify-center items-center py-4 rounded w-full">
+              <div>
+                <p className="text-base leading-4">Pay $54652</p>
+              </div>
+            </button>
+          </div >
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
--- components/float/page.js
+++ components/float/page.js
@@ -14,7 +14,7 @@ const Float = () => {
                     <p className="text-sm font-normal text-gray-200">Welcome to <span className='underline underline-offset-2 decoration-pink-500'>Vintage Vortex</span> E-commerce store. </p>
                 </div>
                 <div className="flex items-center flex-shrink-0">
-                    <Link href={'/signup'} className="px-5 py-2 mr-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300">Sign up</Link>
+                    <Link href={'/login'} className="px-5 py-2 mr-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300">Login</Link>
                     <button data-dismiss-target="#marketing-banner" type="button" className="absolute top-2.5 right-2.5 md:relative md:top-auto md:right-auto flex-shrink-0 inline-flex justify-center items-center text-gray-400  rounded-lg text-sm p-1.5 hover:bg-gray-600 hover:text-white">
                         <svg aria-hidden="true" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         <span className="sr-only">Close banner</span>
