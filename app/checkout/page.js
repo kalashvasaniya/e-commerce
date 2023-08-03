cd8f69ee9b9506dd736d24994c24b08bd2a99113
kalash <kalashvasaniya@gmail.com>
2023-08-03 17:09:40 +0530

    Create Orders page

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -124,7 +124,7 @@ export default function page() {
               <label htmlFor="address" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
             </div>
 
-            <Link href={'/order'}>
+            <Link href={'/orders'}>
               <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Proceed To Pay ₹{subTotal.toFixed(2)}
               </button>
---
+++ app/orders/page.js
@@ -0,0 +1,59 @@
+"use client"
+import React from 'react'
+import { useCart } from '../layout'
+import Image from 'next/image'
+
+
+const orders = () => {
+    const { cart, subTotal } = useCart()
+    return (
+        <>
+            <div className="pt-52 pb-8">
+                <h1 className="text-4xl font-bold text-white text-center">My Orders</h1>
+            </div>
+
+            <section className="">
+                <div className="container px-5 mx-auto">
+                    <div className="flex flex-col">
+                        <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+                            <h2 className="text-sm title-font text-pink-500 tracking-widest">Vintage Vortex</h2>
+                            <div className="flex mb-4">
+                                <a className="flex-grow text-green-500 py-3 text-lg">Confirm your order!</a>
+                            </div>
+                            <div className="grid grid-cols-3 py-3">
+                                <span className="text-red-500 truncate underline underline-offset-2">Product</span>
+                                <span className="ml-auto text-red-500 truncate underline underline-offset-2">Total</span>
+                                <span className="ml-auto text-red-500 truncate underline underline-offset-2">Quantity</span>
+                            </div>
+                            {Object.keys(cart).map((key) => {
+                                return (
+
+                                    <div div key={key} className="border-gray-500" >
+                                        {cart[key].quantity > 0 ? (
+                                            <div className="grid grid-cols-3 border-t py-2">
+                                                <span className="text-gray-100">{cart[key].name} ({cart[key].size}/{cart[key].color})</span>
+                                                <span className="ml-auto text-gray-300">₹{(cart[key].price * cart[key].quantity).toFixed(2)}</span>
+                                                <span className="ml-auto text-gray-300">{cart[key].quantity}</span>
+                                            </div>
+                                        ) : null}
+                                    </div>
+                                )
+                            })}
+                            <hr />
+                            <div className="flex pt-4">
+                                <span className="title-font font-medium text-2xl text-gray-200">Total Payment <span className='text-green-500'>
+                                    ₹{subTotal.toFixed(2)}
+                                </span> For <span className='text-green-500'>
+                                        {Object.keys(cart).reduce((acc, key) => acc + cart[key].quantity, 0)}
+                                    </span> Items</span>
+                                <button className="flex ml-auto text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-700 rounded">Confirm Order</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section >
+        </>
+    )
+}
+
+export default orders
